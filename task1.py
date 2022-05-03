40e87130e5f75d04dfe4b9975ca85ec9b665df64
KoroleffGeorge <georgijkorolev696@gmail.com>
2022-05-03 19:38:51 +0400

    test1

---
+++ task1.py
@@ -0,0 +1,26 @@
+'''GUF'''
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+data = pd.read_csv('udemy_courses.csv')
+fig, ax = plt.subplots(2)
+levels = data['level'].unique()
+avgs = []
+cols1 = []
+for i in levels:
+    level = data.loc[data['level'] == i]
+    avgs.append(level['content_duration'].sum() / len(level['content_duration']))
+    cols1.append(i)
+subjects = data['subject'].unique()
+nums = []
+cols2 = []
+for i in subjects:
+    subject = data.loc[data['subject'] == i]
+    nums.append(len(subject['course_id']))
+    cols2.append(i)
+ax[0].bar(cols1, avgs)
+ax[1].bar(cols2, nums)
+fig.set_figwidth(10)
+plt.show()
+
+
